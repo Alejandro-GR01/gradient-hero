384e30c99d9489cf37b9cfd6710552ff9d026d37
Alejandro <alejandroguzmanr011231@gmail.com>
2026-01-27 21:50:48 -0500

    script-last maniene el aspecto en mobile con rezise()

--- script.js
+++ script.js
@@ -286,7 +286,7 @@ window.addEventListener("load", function () {
   }
   gl.useProgram(program);
 
-  const positions = [-1,-1, 1,-1, -1,1, 1,1];
+  const positions = [-1.0,-1.0, 1,-1.0, -1.0,1.0, 1.0,1.0];
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
